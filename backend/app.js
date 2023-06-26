93db6e96348216e06b8181ffc7fec5c329895ff7
Kyrllan Nogueira <kyrllan@iMac-de-Kyrllan.Dlink>
2023-06-26 17:05:24 -0300

    backend service

---
+++ backend/app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`O Express está rodando na porta ${PORT}`);
+});
+
+// body parser
+app.use(bodyParser.urlencoded({extended: false}));
+
+// db connection
+db.authenticate()
+  .then(() => {
+    console.log('Conectou o banco com sucesso');
+  })
+  .catch((error) => {
+    console.log('Ocorreu um erro ao conectar', error);
+  });
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Está funcionando');
+});
+
+//jobs routes
+app.use('/jobs', require('./routes/jobs'));
